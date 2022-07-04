16f1fb572251b36bd624bb38905cee7fb0a6fc23
Nicko <nicko.chamorro@gmail.com>
2022-07-04 00:37:43 -0300

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -75,11 +75,11 @@ const comprobarAncho = () => {
 const comprobarRotacion = () => {
     if (detector.tablet()!=null || detector.phone()!=null){
         function reorient(e) {
-        var portrait = (window.orientation % 180 == 0);
-        // $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+            var portrait = (window.orientation % 180 == 0);
+            $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
+            window.onorientationchange = reorient;
+            window.setTimeout(reorient, 0);
         }
-        window.onorientationchange = reorient;
-        window.setTimeout(reorient, 0);
     }
 };
 
